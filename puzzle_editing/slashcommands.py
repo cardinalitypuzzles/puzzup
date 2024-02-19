71dcdba899f24c069d2de7d3d027b71e97e310fa
Shuxin Zhan <shuxinzhan@gmail.com>
2024-02-18 23:20:00 -0500

    buff /up info

--- puzzle_editing/slashcommands.py
+++ puzzle_editing/slashcommands.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+from typing import Dict
 
 from discord_interactions import verify_key
 from django.conf import settings
@@ -140,8 +141,23 @@ def archiveChannelHandler(request, payload):
     return JsonResponse(responseJson)
 
 
+def _puzzleInfoHelper(puzzle: Dict) -> str:
+    return (
+        f"Codename: {puzzle['codename']}\n"
+        f"Name: ||{puzzle['name']}||\n"
+        f"This puzzle has status **{puzzle['status']}**!\n"
+        f"Access it at <{puzzle['url']}>\n"
+        f"Brainstorming/solution sheet: <{puzzle['solution']}>\n"
+        f"Author(s): {puzzle['authors']}"
+    )
+
+
 def puzzleInfoHandler(request, payload):
-    puzzle = Puzzle.objects.filter(discord_channel_id=payload["channel_id"])
+    puzzle = (
+        Puzzle.objects.filter(discord_channel_id=payload["channel_id"])
+        .prefetch_related("authors")
+        .order_by("name")
+    )
     responseJson = {"type": 4}
     puzzles = [
         {
@@ -150,6 +166,10 @@ def puzzleInfoHandler(request, payload):
             "codename": p.codename,
             "summary": p.summary,
             "description": p.description,
+            "status": p.get_status_display(),
+            "url": external_puzzle_url(request, p),
+            "solution": p.solution,
+            "authors": p.author_list,
         }
         for p in puzzle
     ]
@@ -158,17 +178,7 @@ def puzzleInfoHandler(request, payload):
         responsetext += ":warning: This puzzle is linked to multiple puzzles!\n"
 
     elif len(puzzles) > 0:
-        responsetext += "\n".join(
-            [
-                "__{} {}__ <https://{}/puzzle/{}> ".format(
-                    p["codename"] or "NO CODENAME",
-                    p["name"],
-                    request.META["HTTP_HOST"],
-                    p["id"],
-                )
-                for p in puzzles
-            ]
-        )
+        responsetext += "\n".join([_puzzleInfoHelper(p) for p in puzzles])
 
     else:
         responsetext += ":information_source: This channel is not linked to any puzzles"
